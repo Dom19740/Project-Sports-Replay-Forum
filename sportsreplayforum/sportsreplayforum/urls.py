12694b9142f83a2cb7021c8d10ebb8ae4d703ca7
Dom19740 <120669022+Dom19740@users.noreply.github.com>
2024-10-01 17:20:34 +0100

    Add login, register to nav menu, but login broken

--- sportsreplayforum/sportsreplayforum/urls.py
+++ sportsreplayforum/sportsreplayforum/urls.py
@@ -5,8 +5,8 @@ from core import views as core_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('login/', core_views.login_view, name='login'),
-    path('register/', core_views.register_view, name='register'),
-    path('logout/', core_views.logout_view, name='logout'),
+    path('login/', core_views.login_view, name='login_view'),
+    path('register/', core_views.register_view, name='register_view'),
+    path('logout/', core_views.logout_view, name='logout_view'),
     path('f1/', include('f1.urls')),
 ]
