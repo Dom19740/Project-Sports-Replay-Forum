1727466d0478f311ff415600b14516584d2a3531
Dom19740 <120669022+Dom19740@users.noreply.github.com>
2024-10-01 15:26:58 +0100

    Setup functionality of home page and f1 main page and nav bar

--- sportsreplayforum/f1/views.py
+++ sportsreplayforum/f1/views.py
@@ -4,7 +4,3 @@ from .models import RaceWeekend, Event
 def race_weekend_list(request):
     race_weekends = RaceWeekend.objects.all()
     return render(request, 'f1/race_weekend_list.html', {'race_weekends': race_weekends})
-
-def event_detail(request, event_id):
-    event = get_object_or_404(Event, id=event_id)
-    return render(request, 'f1/event_detail.html', {'event': event})
--- sportsreplayforum/home/views.py
+++ sportsreplayforum/home/views.py
@@ -2,12 +2,6 @@ from django.shortcuts import render
 from django.views import View
 from django.conf import settings
 
-# Create your views here.
-
-# This is a little complex because we need to detect when we are
-# running in various configurations
-
-
 class HomeView(View):
     def get(self, request):
         print(request.get_host())
@@ -17,4 +11,4 @@ class HomeView(View):
             'installed': settings.INSTALLED_APPS,
             'islocal': islocal
         }
-        return render(request, 'home/index.html', context)
+        return render(request, 'home/main.html', context)
