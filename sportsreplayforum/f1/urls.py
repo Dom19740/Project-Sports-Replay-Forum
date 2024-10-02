ecfff30bfcb2ae40e2538f807b17c0758673456d
Dom19740 <120669022+Dom19740@users.noreply.github.com>
2024-10-02 12:10:16 +0100

    Populate race_list page with races

--- sportsreplayforum/f1/urls.py
+++ sportsreplayforum/f1/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.race_list, name='race_list'), # F1 events list
+    path('events/', views.event_list, name='event_list'),
     path('events/<int:event_id>/rate/', views.rate_event, name='rate_event'),
     path('events/<int:event_id>/results/', views.event_results, name='event_results'),
-    path('events/', views.event_list, name='event_list'),
 ]
