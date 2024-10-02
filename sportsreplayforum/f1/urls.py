0201a42cd9a4ee02262965af01e40eb49d0a6dcd
Dom19740 <120669022+Dom19740@users.noreply.github.com>
2024-10-02 14:33:01 +0100

    Remove unused event vote and results code

--- sportsreplayforum/f1/urls.py
+++ sportsreplayforum/f1/urls.py
@@ -5,7 +5,4 @@ urlpatterns = [
     path('', views.race_list, name='race_list'), # F1 events list
     path('events/', views.event_list, name='event_list'),
     path('races/<int:race_id>/events/', views.race_events, name='race_events'),
-    path('events/<int:event_id>/rate/', views.rate_event, name='rate_event'),
-    path('events/<int:event_id>/results/', views.event_results, name='event_results'),
-    
 ]
