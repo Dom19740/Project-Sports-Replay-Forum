73b2c5c079955b64ad1eb12ba43b5f79bb0b6a69
Dom19740 <120669022+Dom19740@users.noreply.github.com>
2024-10-09 16:53:23 +0100

    Troubleshooting 2nd app functionaliy + Vote button not working

--- sportsreplayforum/sports/nations/urls.py
+++ sportsreplayforum/sports/nations/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from core import views
 
+app_name = 'nations'
 urlpatterns = [
     path('', views.competition_schedule, name='competition_schedule'),
     path('competitions/<int:competition_id>/events/', views.event_list, name='event_list'),
