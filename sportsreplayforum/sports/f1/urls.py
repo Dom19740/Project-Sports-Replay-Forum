b3d93405d5558dfc28b5d1a351b3ba53d4600a0d
Dom19740 <120669022+Dom19740@users.noreply.github.com>
2024-10-12 15:38:18 +0100

    event list working

--- sportsreplayforum/sports/f1/urls.py
+++ sportsreplayforum/sports/f1/urls.py
@@ -3,5 +3,5 @@ from core import views
 
 app_name = 'f1'
 urlpatterns = [
-        path('', views.competition_schedule, {'league': 'Formula 1'}, name='competition_schedule'),
+    path('', views.competition_schedule, {'league': 'Formula 1'}, name='competition_schedule'),
 ]
