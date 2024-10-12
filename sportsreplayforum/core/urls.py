c33cf5bf5b10a326c81b5a5948d5e3316da4e535
Dom19740 <120669022+Dom19740@users.noreply.github.com>
2024-10-12 14:53:06 +0100

    f1 and nations seperate app views and urls fixed

--- sportsreplayforum/core/urls.py
+++ sportsreplayforum/core/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('login/', views.sign_in, name='login'),
     path('logout/', views.sign_out, name='logout'),
     path('register/', views.sign_up, name='register'),
+    path('competition_schedule/', views.competition_schedule, name='competition_schedule'),
     path('event_list/<int:competition_id>/', views.event_list, name='event_list'),
-    path('events/<int:event_id>/', views.event, name='event'),
     path('events/<int:event_id>/vote/', views.vote, name='vote'),
 ]
--- sportsreplayforum/sports/nations/urls.py
+++ sportsreplayforum/sports/nations/urls.py
@@ -3,8 +3,5 @@ from core import views
 
 app_name = 'nations'
 urlpatterns = [
-    path('', views.competition_schedule, name='competition_schedule'),
-    path('competitions/<int:competition_id>/events/', views.event_list, name='event_list'),
-    path('events/<int:event_id>/', views.event, name='event'),
-    path('events/<int:event_id>/vote/', views.vote, name='vote'),
+    path('', views.competition_schedule, {'league': 'UEFA'}, name='competition_schedule'),
 ]
