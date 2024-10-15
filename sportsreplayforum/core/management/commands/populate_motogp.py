901ffc96003c1c4882d89fda41486edc563b31c9
Dom19740 <120669022+Dom19740@users.noreply.github.com>
2024-10-15 17:19:18 +0500

    timezone testing, populate motogp updated to match f1

--- sportsreplayforum/core/management/commands/populate_motogp.py
+++ sportsreplayforum/core/management/commands/populate_motogp.py
@@ -1,8 +1,8 @@
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
+from django.utils import timezone
 from core.models import Competition, Event
-from datetime import datetime
 from dateutil import parser
-import requests, pytz
+import requests
 
 class Command(BaseCommand):
     help = 'Populate the database with Formula 1 competitions and events'
@@ -23,73 +23,46 @@ class Command(BaseCommand):
         for item in data:
         
             competition_name = item['strEvent']
-            competition_date = item['dateEvent']
+            competition_date = parser.isoparse(item['strTimestamp']).date()
 
             # Check if competition already exists
-            try:
-                Competition.objects.get(name=competition_name, date=competition_date)
-            except Competition.DoesNotExist:
-                competition = Competition(
-                    league=item['strLeague'],
-                    name=competition_name,
-                    date=datetime.strptime(competition_date, '%Y-%m-%d').date()
-                )
+            competition, created = Competition.objects.get_or_create(
+                name=competition_name,
+                date=competition_date,
+                defaults={
+                    'league': item['strLeague'],
+                }
+            )
+
+            if created:
                 competition.save()
 
-                date_time = parser.isoparse(item['strTimestamp']).astimezone(pytz.utc)
-                is_finished = (
-                    'Finished' in item['strEvent'] or
-                    item.get('intHomeScore') is not None or
-                    item.get('strVideo') != ""
+            date_time = timezone.make_aware(parser.isoparse(item['strTimestamp']))
+            is_finished = (
+                'Finished' in item['strStatus'] or
+                item.get('strVideo') != ""
+            )
+
+            # Create a race event for the competition
+            race_event, created = Event.objects.get_or_create(
+                event_list=competition,
+                event_type='Race',
+                date_time=date_time,
+                idEvent=item['idEvent'],
+                defaults={
+                    'video_id': item['strVideo'],
+                    'is_finished': is_finished,
+                }
+            )
+
+            if not created:
+                # Update the fields if the event already exists
+                Event.objects.filter(pk=race_event.pk).update(
+                    video_id=item['strVideo'],
+                    is_finished=is_finished,
                 )
 
-                # Create a race event for the competition
-                try:
-                    race_event = Event.objects.get(event_list=competition, event_type='Race', date_time=date_time)
-                except Event.DoesNotExist:
-                    race_event = Event(
-                        event_list=competition,
-                        event_type='Race',
-                        date_time=date_time,
-                        idEvent=item['idEvent'],
-                        video_id=item['strVideo'],
-                        is_finished=is_finished,
-                    )
-                    race_event.save()
-
-        # Step 2: Create Events associated with each Competition
-        for item in data:
-            if item.get('strEvent'):
-                for competition in Competition.objects.all():
-                    # Check for qualifying, sprint, or sprint shootout events
-                    if competition.name in item['strEvent']:
-                        if 'Qualifying' in item['strEvent']:
-                            event_type = 'Qualifying'
-                        elif 'Sprint Shootout' in item['strEvent']:
-                            event_type = 'Sprint Shootout'
-                        elif 'Sprint' in item['strEvent']:
-                            event_type = 'Sprint'
-                        else:
-                            continue  # Skip if not qualifying, sprint, or sprint shootout
-
-                        date_time = parser.isoparse(item['strTimestamp']).astimezone(pytz.utc)
-                        is_finished = (
-                            'Finished' in item['strEvent'] or
-                            item.get('intHomeScore') is not None or
-                            item.get('strVideo') != ""
-                        )
-
-                        try:
-                            event = Event.objects.get(event_list=competition, event_type=event_type, date_time=date_time)
-                        except Event.DoesNotExist:
-                            event = Event(
-                                event_list=competition,
-                                event_type=event_type,
-                                date_time=date_time,
-                                idEvent=item['idEvent'],
-                                video_id=item['strVideo'],
-                                is_finished=is_finished
-                            )
-                            event.save()
+            if created:
+                race_event.save()
 
         self.stdout.write(self.style.SUCCESS("Competitions and events populated successfully"))
