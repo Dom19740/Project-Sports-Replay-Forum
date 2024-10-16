17323d5ff3a3f73400024ec751fea53fc4195b17
Dom19740 <120669022+Dom19740@users.noreply.github.com>
2024-10-16 15:26:01 +0100

    test api returned to previous version

---
+++ sportsreplayforum/core/management/commands/TEST_api1.py
@@ -0,0 +1,28 @@
+import thesportsdb
+import json, requests
+
+"""
+4370&s=2024 F1 2024
+4490 Nations League
+4849 UEFA Champions League
+4328 Premier League
+4380 MotoGP
+4407 Moto GP
+4429 World Cup
+"""
+
+response = requests.get(f"https://www.thesportsdb.com/api/v1/json/3/eventsseason.php?id={4328}")
+
+
+if response.status_code == 200:
+    # Convert the response to JSON format
+    data = response.json()
+    print(data)
+
+    # Save the JSON data to a file
+    with open("TEST_api1.json", "w") as json_file:
+        
+        json.dump(data, json_file, indent=4)  # 'indent=4' formats the JSON for readability
+    
+    print("JSON data saved successfully!")
+    
