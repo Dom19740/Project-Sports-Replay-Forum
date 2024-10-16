eb45c9d30df9cf30b888ad58afd949897b15b302
Dom19740 <120669022+Dom19740@users.noreply.github.com>
2024-10-16 15:17:10 +0100

    admin filters updated to inc league in event list

--- sportsreplayforum/core/admin.py
+++ sportsreplayforum/core/admin.py
@@ -5,20 +5,26 @@ from .models import Competition, Event, Rating
 @admin.register(Competition)
 class CompetitionAdmin(admin.ModelAdmin):
     list_display = ('league', 'name', 'date')
-    list_filter = ('league', 'name', 'date')
+    list_filter = ('league', 'date')
 
 
 # Register the Event model
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
-    list_display = ('event_type', 'event_list', 'date_time', 'idEvent', 'video_id', 'is_finished')
-    list_filter = ('event_type', 'event_list', 'date_time', 'is_finished')
+    list_display = ('event_list', 'date_time', 'get_league', 'event_type', 'idEvent', 'video_id', 'is_finished')
+    list_filter = ('event_list__league', 'event_type', 'date_time', 'is_finished')
+
+    def get_league(self, obj):
+        return obj.event_list.league
+    get_league.short_description = 'League'
+    get_league.admin_order_field = 'event_list__league'
 
 
 # Register the Rating model (optional, just for viewing in admin)
 @admin.register(Rating)
 class RatingAdmin(admin.ModelAdmin):
     list_display = ('event', 'five_stars', 'four_stars', 'three_stars', 'two_stars', 'one_star', 'percentage', 'get_voters')
+    list_filter = ('five_stars', 'four_stars', 'three_stars', 'two_stars', 'one_star', 'percentage')
 
     def get_voters(self, obj):
         return ', '.join([voter.username for voter in obj.voters.all()])
