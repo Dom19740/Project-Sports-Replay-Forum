641030ea05c53f238adcd7a03f4f265bde4d5162
Dom19740 <120669022+Dom19740@users.noreply.github.com>
2024-10-16 18:00:14 +0100

    Email (optional) added to register

--- sportsreplayforum/core/forms.py
+++ sportsreplayforum/core/forms.py
@@ -10,3 +10,6 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model=User
         fields = ['username','email','password1','password2']
+        labels = {
+            'email': 'Email (optional)',
+        }
