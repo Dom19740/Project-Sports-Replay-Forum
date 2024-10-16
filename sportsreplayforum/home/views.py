dca03bd20b301a074ba1d7304430b91b40e782b0
Dom19740 <120669022+Dom19740@users.noreply.github.com>
2024-10-16 18:55:24 +0100

    recent voted and most rated lists added to homepage

--- sportsreplayforum/home/views.py
+++ sportsreplayforum/home/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views import View
 from django.conf import settings
+from django.db.models import ExpressionWrapper
+from django.db import models
+from core.models import Event, Rating
 
 class HomeView(View):
     def get(self, request):
@@ -11,4 +14,24 @@ class HomeView(View):
             'installed': settings.INSTALLED_APPS,
             'islocal': islocal
         }
+
+        # Get the last events to receive a vote
+        recent_ratings = Rating.objects.all().order_by('-id')[:5]
+        recent_voted_events = []
+        for rating in recent_ratings:
+            if rating.event not in recent_voted_events:
+                recent_voted_events.append(rating.event)
+
+        # Get the top most rated events
+        most_rated_events = Event.objects.annotate(
+            total_votes=ExpressionWrapper(
+                models.F('rating__five_stars') + models.F('rating__four_stars') + models.F('rating__three_stars') + models.F('rating__two_stars') + models.F('rating__one_star'),
+                output_field=models.IntegerField()
+            )
+).order_by('-total_votes')[:5]
+        context = {
+            'recent_voted_events': recent_voted_events,
+            'most_rated_events': most_rated_events,
+        }
+
         return render(request, 'home/main.html', context)
