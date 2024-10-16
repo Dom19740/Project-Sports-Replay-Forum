7eae79bdd668bd147575925f67e65195b65fe527
Dom19740 <120669022+Dom19740@users.noreply.github.com>
2024-10-16 15:26:01 +0100

    test api returned to previous version

--- sportsreplayforum/core/management/commands/TEST_api2.py
+++ sportsreplayforum/core/management/commands/TEST_api2.py
@@ -1,26 +1,42 @@
-import thesportsdb
-import json, requests
+import requests
+import json
 
 """
-4370&s=2024 F1 2024
+
+eventresults.php?id=1963827 = Singapore result
+eventresults.php?id=1963830 = USA results
+
+eventsseason.php?id=
+
 4490 Nations League
 4849 UEFA Champions League
 4328 Premier League
 4380 MotoGP
 4407 Moto GP
 4429 World Cup
+
 """
 
-""" results = thesportsdb.events.nextLeagueEvents("4490")
 
-print(results)
+url = "https://www.thesportsdb.com/api/v2/json/schedule/league/4370/2024"
+api_key = 449702
 
-with open("TEST_api2.json", "w") as json_file:
+headers = {
+    "X-API-KEY": f"{api_key}",
+    "Content-Type": "application/json"
+}
+
+response = requests.get(url, headers = headers)
+data = response.json()
+
+if response.status_code == 200:
+    print(data)
+
+    with open("TEST_api.json", "w") as json_file:
+        
+        json.dump(data, json_file, indent=4)  # 'indent=4' formats the JSON for readability
     
-    json.dump(results, json_file, indent=4)  # 'indent=4' formats the JSON for readability
-
-print("JSON data saved successfully!") """
-
-results = thesportsdb.events.eventInfo("1008672")
-
-print(results)
+    print("JSON data saved successfully!")
+    
+else:
+    print(f"Request failed with status code: {response.status_code}")
