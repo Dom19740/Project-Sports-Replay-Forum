140540f3d4fcfa03b08355e61a8ee5e7dcf1a977
dom19740 <dbuzugbe@gmail.com>
2024-10-17 19:31:25 +0100

    Dockerfile restored to original

--- sportsreplayforum/sportsreplayforum/settings.py
+++ sportsreplayforum/sportsreplayforum/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
