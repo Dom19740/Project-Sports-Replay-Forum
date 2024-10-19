9c9c460e4991cd707c4310afbc93591afc218000
dom19740 <dbuzugbe@gmail.com>
2024-10-19 13:51:09 +0100

    profile page built, not working

--- sportsreplayforum/accounts/urls.py
+++ sportsreplayforum/accounts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('login/', views.sign_in, name='login'),
     path('logout/', views.sign_out, name='logout'),
     path('register/', views.sign_up, name='register'),
+    path('profile/', views.profile_view, name='profile'),
 ]
