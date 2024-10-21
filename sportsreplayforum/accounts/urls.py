3ccccebee2567d32d719ae1618da20f045e77fec
dom19740 <dbuzugbe@gmail.com>
2024-10-21 16:13:39 +0100

    Login method updated

--- sportsreplayforum/accounts/urls.py
+++ sportsreplayforum/accounts/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 app_name = 'accounts'
 urlpatterns = [
+    path('register/', views.sign_up, name='register'),
     path('login/', views.sign_in, name='login'),
     path('logout/', views.sign_out, name='logout'),
-    path('register/', views.sign_up, name='register'),
     path('profile/', views.profile_view, name='profile'),
 ]
