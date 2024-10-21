9b4a7c6c99ac47e60801018e389002981c6d3813
dom19740 <dbuzugbe@gmail.com>
2024-10-21 12:56:54 +0100

    new register form implemented

--- sportsreplayforum/accounts/forms.py
+++ sportsreplayforum/accounts/forms.py
@@ -7,12 +7,41 @@ class LoginForm(forms.Form):
     password = forms.CharField(max_length=65, widget=forms.PasswordInput)
 
 class RegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
     class Meta:
-        model=User
-        fields = ['username','email','password1','password2']
-        labels = {
-            'email': 'Email (optional)',
-        }
+        model = User
+        fields = ["username", "email", "password1", "password2"]
+
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].help_text = ''
+        self.fields['email'].required = False
+        self.fields['password1'].help_text = ''
+        self.fields['password2'].help_text = ''
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if User.objects.filter(username=username).exists():
+            self.add_error('username', 'Username already exists')
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if email:
+            if User.objects.filter(email=email).exists():
+                self.add_error('email', 'Email address already in use')
+        return email
+
+    def clean(self):
+        cleaned_data = super().clean()
+        if self.errors:
+            self.fields['password1'].widget.attrs['value'] = self.data.get('password1')
+            self.fields['password2'].widget.attrs['value'] = self.data.get('password2')
+        return cleaned_data
+        
+
 
 
 class UserUpdateForm(UserChangeForm):
