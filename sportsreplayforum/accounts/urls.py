1c825592e2df7a19f782a7e4fcf9024b696e770c
dom19740 <dbuzugbe@gmail.com>
2024-10-22 13:11:59 +0100

    Password reset setup half working

--- sportsreplayforum/accounts/urls.py
+++ sportsreplayforum/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django. contrib. auth import views as auth_views
+from django.contrib.auth import views as auth_views
 from .forms import CustomPasswordResetForm
 from . import views
 
@@ -9,12 +9,11 @@ urlpatterns = [
     path('register/', views.sign_up, name='register'),
     path('login/', views.sign_in, name='login'),
     path('logout/', views.sign_out, name='logout'),
-    path('password-reset/', auth_views.PasswordResetView.as_view(
-        template_name='accounts/password_reset_form.html',
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+        
         form_class=CustomPasswordResetForm,
-        email_template_name='accounts/password_reset_email.html'
     ), name='password_reset'),
-    path('password_reset_done/', auth_views. PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_confirm'),
-    path('password_reset_complete/', auth_views. PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('password_reset_done/', auth_views. PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html',), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views. PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'), name='password_reset_complete'),
 ]
--- sportsreplayforum/sportsreplayforum/settings.py
+++ sportsreplayforum/sportsreplayforum/settings.py
@@ -211,7 +211,8 @@ SITE_ID = 1
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
-EMAIL_PORT = 587
+""" EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS') """
+EMAIL_HOST_USER = 'dbuzugbe@gmail.com'
+EMAIL_HOST_PASSWORD = 'mmdczuqebmhpkhrj'
 EMAIL_USE_TLS = True
