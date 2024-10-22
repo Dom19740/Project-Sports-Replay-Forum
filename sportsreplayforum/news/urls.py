ec312aed276cb14a2bf0d2c39578ceaef264fb18
dom19740 <dbuzugbe@gmail.com>
2024-10-22 15:58:58 +0100

    git ignore updated

--- sportsreplayforum/news/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import news_sidebar
-
-urlpatterns = [
-    path('news/', news_sidebar, name='news_sidebar'),
-]
--- sportsreplayforum/news/views.py
+++
@@ -1,8 +0,0 @@
-# news/views.py
-
-from django.shortcuts import render
-from .services import fetch_news
-
-def news_sidebar(request):
-    news_articles = fetch_news()
-    return render(request, 'news/sidebar.html', {'news_articles': news_articles})
