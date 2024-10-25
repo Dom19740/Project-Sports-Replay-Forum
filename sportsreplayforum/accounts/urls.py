39e04d4d0ae6d76ca9a163573f92910c8c788645
dom19740 <dbuzugbe@gmail.com>
2024-10-25 13:17:01 +0100

    password reset working

--- sportsreplayforum/accounts/urls.py
+++ sportsreplayforum/accounts/urls.py
@@ -12,13 +12,14 @@ urlpatterns = [
         template_name='accounts/password_reset_form.html',
         form_class=CustomPasswordResetForm,
         email_template_name='accounts/password_reset_email.html',
-        success_url=reverse_lazy('accounts:password_reset_done')  # Corrected here
+        success_url=reverse_lazy('accounts:password_reset_done')
     ), name='password_reset'),
     path('password-reset-done/', auth_views.PasswordResetDoneView.as_view(
         template_name='accounts/password_reset_done.html'
     ), name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-        template_name='accounts/password_reset_confirm.html'
+        template_name='accounts/password_reset_confirm.html',
+        success_url=reverse_lazy('accounts:password_reset_complete')  # Added this line
     ), name='password_reset_confirm'),
     path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
         template_name='accounts/password_reset_complete.html'
