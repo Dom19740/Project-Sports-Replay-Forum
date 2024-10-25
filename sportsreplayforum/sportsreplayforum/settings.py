0a45614fef1eda1b86d7c5b428b07a8446f1912b
dom19740 <dbuzugbe@gmail.com>
2024-10-25 14:01:48 +0100

    nav bar title capitalised

--- sportsreplayforum/sportsreplayforum/settings.py
+++ sportsreplayforum/sportsreplayforum/settings.py
@@ -114,8 +114,8 @@ DATABASES = {
 }
 
 # render database
-#database_url = os.environ.get('DATABASE_URL')
-#DATABASES['default'] = dj_database_url.parse(database_url)
+database_url = os.environ.get('DATABASE_URL')
+DATABASES['default'] = dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
