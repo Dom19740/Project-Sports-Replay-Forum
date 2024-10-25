3f14f4f937790505e09510eeaa31fd18ab6c2e64
dom19740 <dbuzugbe@gmail.com>
2024-10-25 13:24:00 +0100

    Implemented password reset functionality

--- sportsreplayforum/accounts/urls.py
+++ sportsreplayforum/accounts/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     ), name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
         template_name='accounts/password_reset_confirm.html',
-        success_url=reverse_lazy('accounts:password_reset_complete')  # Added this line
+        success_url=reverse_lazy('accounts:password_reset_complete')
     ), name='password_reset_confirm'),
     path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
         template_name='accounts/password_reset_complete.html'
