87e7e43ed68316272f0397e1762cdd2bccc84c81
dom19740 <dbuzugbe@gmail.com>
2024-10-25 12:14:06 +0100

    password-reset-done working

--- sportsreplayforum/accounts/urls.py
+++ sportsreplayforum/accounts/urls.py
@@ -1,8 +1,7 @@
-from django.urls import path
-from django. contrib. auth import views as auth_views
+from django.urls import path, reverse_lazy
+from django.contrib.auth import views as auth_views
 from .forms import CustomPasswordResetForm
 from . import views
-
 
 app_name = 'accounts'
 urlpatterns = [
@@ -12,9 +11,16 @@ urlpatterns = [
     path('password-reset/', auth_views.PasswordResetView.as_view(
         template_name='accounts/password_reset_form.html',
         form_class=CustomPasswordResetForm,
-        email_template_name='accounts/password_reset_email.html'
+        email_template_name='accounts/password_reset_email.html',
+        success_url=reverse_lazy('accounts:password_reset_done')  # Corrected here
     ), name='password_reset'),
-    path('password_reset_done/', auth_views. PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_confirm'),
-    path('password_reset_complete/', auth_views. PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('password-reset-done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='accounts/password_reset_done.html'
+    ), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='accounts/password_reset_confirm.html'
+    ), name='password_reset_confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='accounts/password_reset_complete.html'
+    ), name='password_reset_complete'),
 ]
