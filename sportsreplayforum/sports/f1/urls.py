bcd5e6945b44331955ac701af6ce7ec89c00edd8
dom19740 <dbuzugbe@gmail.com>
2024-11-14 17:30:05 +0000

    sports apps removed, not used

--- sportsreplayforum/sports/f1/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from core import views
-
-app_name = 'f1'
-urlpatterns = [
-    path('', views.competition_schedule, {'league': 'Formula 1'}, name='competition_schedule'),
-]
--- sportsreplayforum/sports/premier/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from core import views
-
-app_name = 'premier'
-urlpatterns = [
-    path('', views.competition_schedule, {'league': 'English Premier League'}, name='competition_schedule'),
-]
