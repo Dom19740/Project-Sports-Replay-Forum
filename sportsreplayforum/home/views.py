ee2d945272465494fe5d3ca2351cc4bb81f8ba63
dom19740 <dbuzugbe@gmail.com>
2024-11-14 19:36:28 +0000

    recently rated and most recent events formatted

--- sportsreplayforum/home/views.py
+++ sportsreplayforum/home/views.py
@@ -16,21 +16,36 @@ class HomeView(View):
         # Get the last events to receive a vote
         recent_ratings = Rating.objects.all().order_by('-id')[:5]
         recent_voted_events = []
+        
         for rating in recent_ratings:
-            if rating.event not in recent_voted_events:
-                recent_voted_events.append(rating.event)
+            event = rating.event
+            league = event.event_list.league  # Get the league for this event
+            recent_voted_events.append({
+                'event': event,
+                'league': league
+        })
 
         # Get the next 5 events that will occur
         now = datetime.now()
         last_events = Event.objects.filter(date_time__lte=now).order_by('-date_time')[:5]
+        last_events_list = []
+        
+        for event in last_events:
+            league = event.event_list.league  # Get the league for this event
+            last_events_list.append({
+                'event': event,
+                'league': league
+        })
 
         context = {
             'installed': settings.INSTALLED_APPS,
             'islocal': islocal,
             'recent_voted_events': recent_voted_events,
-            'last_events': last_events,
+            'last_events': last_events_list,
         }
+        
         return render(request, 'home/home.html', context)
+    
 
 def about_view(request):
     return render(request, 'home/about.html')
