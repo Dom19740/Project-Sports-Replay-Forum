8bf468134cc0ec14bfebad896958d531bef23f0f
dom19740 <dbuzugbe@gmail.com>
2024-11-16 13:18:33 +0000

    scroll bars working and styled

--- sportsreplayforum/home/views.py
+++ sportsreplayforum/home/views.py
@@ -3,57 +3,47 @@ from django.views import View
 from django.conf import settings
 from django.utils import timezone
 from datetime import datetime, timedelta
+from django.http import JsonResponse
 from core.models import Event, Rating
 from core.views import sports
-from django.core.paginator import Paginator
 
 class HomeView(View):
     def get(self, request):
-        print(request.get_host())
-        host = request.get_host()
-        islocal = host.find('localhost') >= 0 or host.find('127.0.0.1') >= 0
-
-        # Get the last events to receive a vote
-        recent_ratings = Rating.objects.all().order_by('-id')[:6]
-        recent_voted_events = []
-        
-        for rating in recent_ratings:
-            event = rating.event
-            league = event.event_list.league  # Get the league for this event
-            recent_voted_events.append({
-                'event': event,
-                'league': league
-        })
-
         # Get todays events
         now = timezone.now()
         today = now.date()
         tomorrow = today + timedelta(days=1)
 
-        last_events = Event.objects.filter(date_time__range=(datetime.combine(today, datetime.min.time()), datetime.combine(tomorrow, datetime.min.time()))).order_by('date_time')
+        recent_ratings = Rating.objects.all().order_by('-id')[:10]
+        recent_voted_events = []
 
-        paginator = Paginator(last_events, 6)
-        page_number = request.GET.get('page')
-        page_obj = paginator.get_page(page_number)
-
-        last_events_list = [{'event': event, 'league': event.event_list.league} for event in page_obj]
-
-        for event in last_events:
+        for rating in recent_ratings:
+            event = rating.event
             league = event.event_list.league  # Get the league for this event
-            last_events_list.append({
+            recent_voted_events.append({
                 'event': event,
-                'league': league
+                'league': league,
         })
+
+        todays_events = Event.objects.filter(date_time__range=(datetime.combine(today, datetime.min.time()), datetime.combine(tomorrow, datetime.min.time()))).order_by('date_time')
 
         context = {
             'installed': settings.INSTALLED_APPS,
-            'islocal': islocal,
             'recent_voted_events': recent_voted_events,
-            'page_obj': page_obj,
+            'todays_events': todays_events,
         }
         
         return render(request, 'home/home.html', context)
-    
+
+
+def events_more(request):
+    # Get the next batch of events
+    offset = int(request.GET.get('offset', 0))
+    events = Event.objects.filter(date_time__gte=timezone.now()).order_by('date_time')[offset:offset+10]
+
+    # Return a JSON response
+    return JsonResponse({'events': [{'id': event.id, 'event_list': event.event_list.name, 'event': event.event, 'event_type': event.event_type, 'date_time': event.date_time} for event in events]})
+
 
 def about_view(request):
     return render(request, 'home/about.html')
