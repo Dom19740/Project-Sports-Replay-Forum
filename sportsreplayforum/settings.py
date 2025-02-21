69732b8e0d55a9fb16ca3cb9b86b7850b7ec8b66
Dom19740 <dbuzugbe@gmail.com>
2025-02-21 17:09:33 +0000

    django logs added

--- sportsreplayforum/settings.py
+++ sportsreplayforum/settings.py
@@ -243,3 +243,23 @@ AUTHENTICATION_BACKENDS = [
 TEMPLATE_CONTEXT_PROCESSORS = [
     'sportsreplayforum.core.context_processors.sports',
 ]
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': '/home/dom974/var/www/sportsforum/djangologs/django_error.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
