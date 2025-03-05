1add06fcac9cf6b4755523a194d1d4c60b18b9b7
Dom19740 <dbuzugbe@gmail.com>
2025-03-05 15:12:02 +0000

    set_site_domain.py added

---
+++ core/management/commands/set_site_domain.py
@@ -0,0 +1,32 @@
+from django.core.management.base import BaseCommand
+from django.conf import settings
+from django.contrib.sites.models import Site
+
+class Command(BaseCommand):
+    help = 'Set the site domain and name'
+
+    def handle(self, *args, **kwargs):
+        if settings.DJANGO_ENV == 'local':
+            Site.objects.update_or_create(
+                id=1,
+                defaults={
+                    'domain': '127.0.0.1:8000',
+                    'name': 'Local Site'
+                }
+            )
+        else:
+            Site.objects.update_or_create(
+                id=1,
+                defaults={
+                    'domain': 'shouldiwatchsports.com',
+                    'name': 'Should I Watch Sports'
+                }
+            )
+            Site.objects.update_or_create(
+                id=2,
+                defaults={
+                    'domain': 'www.shouldiwatchsports.com',
+                    'name': 'Should I Watch Sports'
+                }
+            )
+        self.stdout.write(self.style.SUCCESS('Successfully set site domain and name'))
