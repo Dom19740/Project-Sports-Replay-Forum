bdaf7c971e92ab3e4c1c0e2f302757a13e3b9d2c
Dom19740 <dbuzugbe@gmail.com>
2025-03-13 12:35:18 +0000

    comment time stamp improved

--- core/templatetags/event_tags.py
+++ core/templatetags/event_tags.py
@@ -11,3 +11,22 @@ def is_past(event):
 @register.filter
 def voting_closed(event):
     return event.date_time + timedelta(days=7) <= timezone.now()
+
+@register.filter
+def format_comment_date(value):
+    if not value:
+        return ''
+    now = timezone.now()
+    diff = now - value
+    seconds = diff.total_seconds()
+
+    if seconds < 24 * 3600:
+
+        if seconds < 3600:
+            minutes = int(seconds // 60)
+            return f"{minutes}m ago" if minutes != 1 else "1m ago"
+        else:
+            hours = int(seconds // 3600)
+            return f"{hours}h ago" if hours != 1 else "1h ago"
+
+    return value.strftime("%b %d %Y")
