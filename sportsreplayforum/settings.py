077d907726249996732a3fe696bed0435ff7a520
Dom19740 <dbuzugbe@gmail.com>
2026-06-18 14:23:20 +0700

    reset password email fix

--- sportsreplayforum/settings.py
+++ sportsreplayforum/settings.py
@@ -236,7 +236,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = mail
 EMAIL_HOST_PASSWORD = mail_pass
-DEFAULT_FROM_EMAIL = 'SIWS <dom@dpbcreative.com>'
+DEFAULT_FROM_EMAIL = f'SIWS <{mail}>'
 EMAIL_SUBJECT_PREFIX = "Password Recovery"
 
 
